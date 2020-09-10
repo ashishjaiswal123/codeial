2949389ea5fa05a73d6baa99dfc862e34356cb32
ashish jaiswal <jaiswalashish025@gmail.com>
2020-09-11 00:24:02 +0530

    created sign-in, sign-out,profile session and logging out session

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,9 +1,19 @@
 const User = require('../models/user');
 
 module.exports.profile = function(req,res){
-    return res.render('user_profile',{
-        title : 'user profile'
-    });
+    if(req.cookies.user_id){
+        User.findById(req.cookies.user_id,function(err,user){
+            if(user){
+                return res.render('user_profile',{
+                    title :"User Profile",
+                    user : user
+                })
+            }
+            return res.redirect('/user/sign-in');
+        });
+    }else{
+        return res.redirect('/user/sign-in');
+    }
 }
 
 //render the signup page
@@ -40,5 +50,30 @@ module.exports.create = function(req,res){
 
 //sign in and create a session
 module.exports.createSession = function(req,res){
-    //todo later
+
+    //steps to authenticate
+    //find the user
+    User.findOne({email:req.body.email},function(err,user){
+        if(err){console.log('errror in finding user in signing in'); return;}
+         //handel user found
+         if(user){
+               //handel password doesn't match
+               if(user.password !=  req.body.password){
+                   return res.redirect('back');
+               }
+
+              //handel session creation
+              res.cookie('user_id',user.id);
+              return res.redirect('/user/profile');
+         }else{
+             //handel user not found
+             return res.redirect('back');
+        }
+    });
+}
+
+//logging out session
+module.exports.logOut = function(req,res){
+    res.cookie('user_id','0');
+    return res.redirect('/user/sign-in');
 }
--- routes/user.js
+++ routes/user.js
@@ -7,6 +7,10 @@ router.get('/profile',userController.profile);
 
 router.get('/sign-up',userController.signUp);
 router.get('/sign-in',userController.singnIn);
+
+router.get('/logging-out-session',userController.logOut);
+
 router.post('/create',userController.create);
+router.post('/create-session',userController.createSession);
 
 module.exports= router;
