4e1132563cde2c946e864c7c1b97df425d1816cd
ashish jaiswal <jaiswalashish025@gmail.com>
2020-09-18 21:24:30 +0530

    async await, connect-flash, notyjs

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -1,27 +1,34 @@
 const Post = require('../models/post');
 const Comment = require('../models/comment');
 
-module.exports.create = function(req,res){
-    console.log(req.user);
-    Post.create({
+module.exports.create = async function(req,res){
+    try{
+        // console.log(req.user);
+    await Post.create({
         content : req.body.content,
         user : req.user._id
-    },function(err,post){
-        if(err){console.log('error in creating a post'); return;}
-        return res.redirect('back');
     });
+    req.flash('success','Post published!!!');
+    return res.redirect('back');
+    }
+    catch(err){
+       console.log('errr',err);
+       req.flash('error','error in posting post');
+    }
+    
 }
 
-module.exports.destroy = function(req,res){
-    Post.findById(req.params.id,function(err,post){
-        // .id means converting the object id into string
-        if(post.user == req.user.id){
-            post.remove();
-            Comment.deleteMany({post: req.params.id}, function(err){
-                return res.redirect('back');
-            })
-        }else{
-            return res.redirect('back');
-        }
-    });
+module.exports.destroy = async function(req,res){
+    
+    let post = await Post.findById(req.params.id);
+    if(post.user == req.user.id){
+        post.remove();
+        await Comment.deleteMany({post: req.params.id});
+        req.flash('success','Post Deleted!!!');
+        return res.redirect('back');
+    }else{
+        req.flash('error', 'error in deleting post');
+        return res.redirect('back');
+       
+    }
 }
